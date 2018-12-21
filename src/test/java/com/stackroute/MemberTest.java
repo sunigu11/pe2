3b31b02a5516d6a1fe5682b2952cafc6f1ae415e
sunigu11 <sunilgupta0739@gmail.com>
2018-12-21 13:05:15 +0530

    initial commit

---
+++ src/test/java/com/stackroute/MemberTest.java
@@ -0,0 +1,22 @@
+package com.stackroute;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MemberTest {
+
+    Member member = null;
+
+    @Before
+    public void setUp(){
+        member = new Member("Harry Potter",30,2500.3);
+    }
+
+    public void testDisplay(){
+
+        //act
+        String expected =
+    }
+
+}
